1181f98f0a4aed40d143d899797ee7f0df1d0e36
Gonzi14 <gonzaloandrade142@gmail.com>
2022-09-25 16:48:02 -0300

    Lo mejoro

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-num = True
-laLista = ""
-'''creo una variable que sea la lista donde guardo todos los numeros'''
-
-
 def contacion(elNumerito, listovich):
-    elNumeroPeroEnNumero = int(float(elNumerito))
-    for i in range(1, elNumeroPeroEnNumero):
+    for i in range(1, int(elNumerito)):
         listovich += str(i) + " "
     listovich += elNumerito
     '''basicamente aca lo que digo es que vaya sumando uno hasta llegar al numero deseado'''
-    return print(listovich)
+    print(listovich)
 
 
-if num:
-    corremelo = input("A que numero queres contar?")
-    contacion(corremelo, laLista)
+contacion(input("A que numero queres contar? "), "")
